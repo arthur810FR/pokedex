e66d5d11383260547a14d29823b80d8bfc2de1ae
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-01-09 11:44:54 -0300

    adicionando a função para converter tios de pokemon em lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -7,8 +11,7 @@ function convertPokemonToLi(pokemon) {
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
                     </ol>
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
                 </div>
