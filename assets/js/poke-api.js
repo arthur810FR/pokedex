f437eba784f5ec3ef4ce4bf6c98d3767cff53529
arthur810FR <arthurfranciscoazevedo@gmail.com>
2025-01-13 08:40:39 -0300

    criando um modelo de pokemon com atributos

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -12,8 +12,5 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => {
-            console.log(pokemonsDetails)
-        })
-        .catch((error) => console.error(error))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
